95803e08d870f2147f682e78363bbd10fa845cbd
Marine ROSSI <marine.rossi11@gmail.com>
2023-12-06 17:17:36 +0100

    render database from event

--- src/components/AllEvents.jsx
+++ src/components/AllEvents.jsx
@@ -25,6 +25,16 @@ function AllEvent() {
         return (
           <div key={i}>
             <Link to={`/events/${event._id}`}> {event._id}</Link>
+            <h2>{event.nameOfTheEvent}</h2>
+            <h2>{event.location}</h2>
+            <h2>{event.date}</h2>
+            <h2>{event.organizer}</h2>
+            <h2>{event.price}</h2>
+            <h2>{event.isPetFriendly && <img src="pet_app.avif" alt="Pet Friendly" />}</h2>
+            <h2>{event.isChildFriendly && <img src="baby_app.png" alt = "Kid Friendly"/>}</h2>
+            <h2>{event.isEcoFriendly && <img src = "ecofriendly_app" alt="Eco Friendly"/>}</h2>
+            <h2>{event.isAccessibilityFriendly && <img src="handicap_app" alt="Accessibility Friendly"/>}</h2>
+            <h2>{event.isVeganFriendly && <img src="vegan_app" alt="Vegan Friendly"/>}</h2>
           </div>
         );
       })}
