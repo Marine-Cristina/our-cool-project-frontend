54d6b9ff9998c8cd708503fa571dc4e95e277da3
Cristina RP <cristinarp95@gmail.com>
2023-12-08 20:25:40 +0100

    Update Details of Business and Event components

--- src/pages/DetailsBusinessPage.jsx
+++ src/pages/DetailsBusinessPage.jsx
@@ -1,30 +1,48 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import accessibility from "/accessibility.png";
+import child from "/child.png";
+import eco from "/eco.png";
+import pet from "/pet.avif";
+import vegan from "/vegan.png";
+
+const BUSINESS_URL = "http://localhost:5005/businesses";
 
 function DetailsBusiness(){
-   const [businessDetails, setBusinessDetails]= useState({}); 
+   const [businessDetails, setBusinessDetails]= useState({});
+   const {businessId} = useParams();
 
-   useEffect(()=>{
+   useEffect(() => {
     axios
-    .get("http://localhost:5005/detailsbusiness")
-    .then (response => setBusinessDetails(response.data))
-    .catch (error => conlonge.log(error)); 
-   },[])
+        .get(`${BUSINESS_URL}/${businessId}`)
+        .then ((response) => {
+            setBusinessDetails(response.data);
+        })
+        .catch ((error) => {
+            console.log(error);
+        });
+    }, [businessId]);
 
    return (
-
     <div>
-        {businessDetails.name}
-        {businessDetails.location}
-        {businessDetails.coordinates}
-        {businessDetails.typeOfBusiness}
-        {businessDetails.isPetFriendly}
-        {businessDetails.isChildFriendly}
-        {businessDetails.isAccessibilityFriendly}
-        {businessDetails.isVeganFriendly}
+        <h1>{businessDetails.name}</h1>
+        <h2>{businessDetails.typeOfBusiness}</h2>
+        <h2>Located in {businessDetails.location}</h2>
+        {businessDetails.coordinates && (
+            <div>
+                <h3>Coordinates:</h3>
+                <p>Latitude: {businessDetails.coordinates.latitude}</p>
+                <p>Longitude: {businessDetails.coordinates.longitude}</p>
+            </div>
+        )}
+        {businessDetails.isPetFriendly && <img src={pet} alt="Pet Friendly" />}
+        {businessDetails.isChildFriendly && <img src={child} alt = "Kid Friendly"/>}
+        {businessDetails.isEcoFriendly && <img src={eco} alt="Eco Friendly"/>}
+        {businessDetails.isAccessibilityFriendly && <img src={accessibility} alt="Accessibility Friendly"/>}
+        {businessDetails.isVeganFriendly && <img src={vegan} alt="Vegan Friendly"/>}
     </div>
-   )
+   );
 }
 
-export default DetailsBusiness
+export default DetailsBusiness;
--- src/pages/DetailsEventsPage.jsx
+++ src/pages/DetailsEventsPage.jsx
@@ -1,31 +1,47 @@
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import accessibility from "/accessibility.png";
+import child from "/child.png";
+import eco from "/eco.png";
+import pet from "/pet.avif";
+import vegan from "/vegan.png";
+
+const EVENTS_URL = "http://localhost:5005/events";
 
 function DetailsEvent(){
-   const [eventDetails, seteventDetails]= useState({}); 
+   const [eventDetails, setEventDetails]= useState({});
+   const {eventId} = useParams();
 
    useEffect(()=>{
     axios
-    .get("http://localhost:5005/detailsevent")
-    .then (response => setEventDetails(response.data))
-    .catch (error => console.log(error)); 
-   },[])
+        .get(`${EVENTS_URL}/${eventId}`)
+        .then ((response) => {
+            setEventDetails(response.data);
+        })
+        .catch ((error) => {
+            console.log(error)
+        }); 
+    }, [eventId]);
 
    return (
-
     <div>
-        {eventDetails.name}
-        {eventDetails.location}
-        {eventDetails.coordinates}
-        {eventDetails.typeOfBusiness}
-        {eventDetails.isPetFriendly}
-        {eventDetails.isChildFriendly}
-        {eventDetails.isAccessibilityFriendly}
-        {eventDetails.isVeganFriendly}
+        <h1>{eventDetails.name}</h1>
+        <h2>{eventDetails.location}</h2>
+        {eventDetails.coordinates && (
+            <div>
+                <h3>Coordinates:</h3>
+                <p>Latitude: {eventDetails.coordinates.latitude}</p>
+                <p>Longitude: {eventDetails.coordinates.longitude}</p>
+            </div>
+        )}
+        {eventDetails.isPetFriendly && <img src={pet} alt="Pet Friendly" />}
+        {eventDetails.isChildFriendly && <img src={child} alt = "Kid Friendly"/>}
+        {eventDetails.isEcoFriendly && <img src={eco} alt="Eco Friendly"/>}
+        {eventDetails.isAccessibilityFriendly && <img src={accessibility} alt="Accessibility Friendly"/>}
+        {eventDetails.isVeganFriendly && <img src={vegan} alt="Vegan Friendly"/>}
     </div>
-   )
+   );
 }
 
-export default DetailsEvents
+export default DetailsEvent;
