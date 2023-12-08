9ea37881b9a0266a86c323aec62a26a2fb2dbb09
Cristina RP <cristinarp95@gmail.com>
2023-12-08 20:25:03 +0100

    Update Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,40 @@
-import { useState } from 'react'
-import './App.css'
-import HomePage from "./pages/HomePage"
-import { Route, Routes } from 'react-router-dom'
-import DetailsBusinessPage from './pages/DetailsBusinessPage'
-import CreateBusiness from './components/CreateBusiness'
-import CreateEvents from './components/CreateEvents'
-import DetailsEventsPage from './pages/DetailsEventsPage'
-import DeleteBusiness from './components/DeleteBusiness'
-import FilterBusiness from './components/FilterBusinesses'
-import FilterEvent from './components/FilterEvents'
-
+import { useState } from "react";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+import DetailsBusinessPage from "./pages/DetailsBusinessPage";
+import CreateBusiness from "./components/CreateBusiness";
+import CreateEvents from "./components/CreateEvents";
+import DetailsEventsPage from "./pages/DetailsEventsPage";
+import DeleteBusiness from "./components/DeleteBusiness";
+import FilterBusiness from "./components/FilterBusinesses";
+import FilterEvent from "./components/FilterEvents";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import DetailsBusinessPage from "./pages/DetailsBusinessPage";
+import DetailsEventsPage from "./pages/DetailsEventsPage";
+import CreateBusiness from "./components/CreateBusiness";
+import CreateEvents from "./components/CreateEvents";
 
 function App() {
-  console.log('hello app')
   return (
-    <Routes>
-      <Route path ="/" element={<HomePage/>}></Route>
-      <Route path = "/businesses" element = {<CreateBusiness/>}></Route>
-      <Route path="/events" element={<CreateEvents/>}></Route>
-      <Route path="/detailsbusiness" element={<DetailsBusinessPage/>}></Route>
-      <Route path="/detailevents" element={<DetailsEventsPage/>}></Route>
-      <Route path="/deletebusiness" element={<DeleteBusiness/>}></Route>
-      <Route path="/filterevent" element={<FilterEvent/>}></Route>
-      <Route path="/filterbusiness" element={<FilterBusiness/>}></Route>
-       </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/businesses" element={<CreateBusiness />} />
+        <Route path="/events" element={<CreateEvents />} />
+        <Route
+          path="/businesses/:businessId"
+          element={<DetailsBusinessPage />}
+        />
+        <Route path="/events/:eventId" element={<DetailsEventsPage />} />
+        <Route path="/deletebusiness" element={<DeleteBusiness />} />
+        <Route path="/filterevent" element={<FilterEvent />} />
+        <Route path="/filterbusiness" element={<FilterBusiness />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
